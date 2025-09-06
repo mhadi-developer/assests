38fb8943486cc6f344576d9187c6e8694e0ed59c
Muhammad Hadi <hadiquershi08@gmail.com>
2025-09-07 01:24:23 +0500

    New dosctor profile

--- js/app.js
+++ js/app.js
@@ -6,11 +6,11 @@ $('.owl-carousel').owlCarousel({
         0:{
             items:1
         },
-        600:{
-            items:3
+        768:{
+            items:2
         },
-        1000:{
-            items:5
+        1024:{
+            items:3
         }
     }
 })
